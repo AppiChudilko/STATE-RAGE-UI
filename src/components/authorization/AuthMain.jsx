893586f2a1bcc22cd26f0b22956552532cd29998
LetikGit <daniil-letovalcev@yandex.ru>
2020-12-14 13:35:59 +0300

    fix: accountmenu fixes

--- src/components/authorization/AuthMain.jsx
+++ src/components/authorization/AuthMain.jsx
@@ -10,7 +10,7 @@ class AuthMain extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            show: true,
+            show: false,
             path: '/'
         }
     }
