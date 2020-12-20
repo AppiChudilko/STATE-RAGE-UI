db8241cb027a4cd808836b6cff08a161e3937ad7
LetikGit <daniil-letovalcev@yandex.ru>
2020-12-21 01:16:18 +0300

    fix: some fixes

--- src/components/accountmenu/AccountMenu.jsx
+++ src/components/accountmenu/AccountMenu.jsx
@@ -8,7 +8,7 @@ class AccountMenu extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            show: true,
+            show: false,
             panels: [
                 {name: 'Общее', id: 'accmenu-main'},
                 {name: 'Имущество', id: 'accmenu-property'},
